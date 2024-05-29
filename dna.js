584413c726151473c84777beacc28c1a8e6c396a
hao-chengy <s11361@kcis.com.tw>
2024-05-29 08:54:55 +0800

    Update dna.js

--- dna.js
+++ dna.js
@@ -1,1 +1,17 @@
-var dna = prompt("Input sequence");
+var dna = "ATGCGAGGDTTC";
+var rna = "";
+for(var i = 0; i < dna.length; i++){
+    if(dna.charAt(i)=="A"){
+        rna+="U";
+    }
+    if(dna.charAt(i)=="T"){
+        rna+="A";
+    }
+    if(dna.charAt(i)=="G"){
+        rna+="C";
+    }
+    if(dna.charAt(i)=="C"){
+        rna+="G";
+    }
+}
+console.log(rna);
