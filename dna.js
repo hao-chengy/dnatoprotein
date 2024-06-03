3179b86207fbf4525e6f1184426234a2015c4b0b
hao-chengy <s11361@kcis.com.tw>
2024-06-03 11:16:40 +0800

    Update dna.js

--- dna.js
+++ dna.js
@@ -1,5 +1,8 @@
-var dna = "ATGCGAGGDTTC";
+var dna = "ATGCGAGGDTTCT";
 var rna = "";
+
+
+
 for(var i = 0; i < dna.length; i++){
     if(dna.charAt(i)=="A"){
         rna+="U";
@@ -15,3 +18,24 @@ for(var i = 0; i < dna.length; i++){
     }
 }
 console.log(rna);
+
+if(rna.length%3!=0){
+    console.log("Can not translate to codon.")
+}else{
+    var codon = "";
+    var count = 0;
+    var num = 0;
+    while(num < rna.length){
+        if(count <3){
+            codon+= rna.charAt(num);
+            count++;
+            num++;
+        }else{
+            codon+= " ";
+            count = 0;
+        }
+        
+    }
+
+    console.log(codon);
+}
