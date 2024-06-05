74e075cc78108801562b4afab7286a0797132001
hao-chengy <s11361@kcis.com.tw>
2024-06-05 09:14:03 +0800

    Update dna.js

--- dna.js
+++ dna.js
@@ -1,43 +1,43 @@
 var dna = "ATGCGAGGDTTCT";
 var rna = "";
 const codons = [
-    "TTT", "TTC", "TTA", "TTG", 
-    "TCT", "TCC", "TCA", "TCG", 
-    "TAT", "TAC", "TAA", "TAG", 
-    "TGT", "TGC", "TGA", "TGG", 
-    "CTT", "CTC", "CTA", "CTG", 
-    "CCT", "CCC", "CCA", "CCG", 
-    "CAT", "CAC", "CAA", "CAG", 
-    "CGT", "CGC", "CGA", "CGG", 
-    "ATT", "ATC", "ATA", "ATG", 
-    "ACT", "ACC", "ACA", "ACG", 
-    "AAT", "AAC", "AAA", "AAG", 
-    "AGT", "AGC", "AGA", "AGG", 
-    "GTT", "GTC", "GTA", "GTG", 
-    "GCT", "GCC", "GCA", "GCG", 
-    "GAT", "GAC", "GAA", "GAG", 
+    "TTT", "TTC", "TTA", "TTG",
+    "TCT", "TCC", "TCA", "TCG",
+    "TAT", "TAC", "TAA", "TAG",
+    "TGT", "TGC", "TGA", "TGG",
+    "CTT", "CTC", "CTA", "CTG",
+    "CCT", "CCC", "CCA", "CCG",
+    "CAT", "CAC", "CAA", "CAG",
+    "CGT", "CGC", "CGA", "CGG",
+    "ATT", "ATC", "ATA", "ATG",
+    "ACT", "ACC", "ACA", "ACG",
+    "AAT", "AAC", "AAA", "AAG",
+    "AGT", "AGC", "AGA", "AGG",
+    "GTT", "GTC", "GTA", "GTG",
+    "GCT", "GCC", "GCA", "GCG",
+    "GAT", "GAC", "GAA", "GAG",
     "GGT", "GGC", "GGA", "GGG"
-  ];
-  
-for(var i = 0; i < dna.length; i++){
-    if(dna.charAt(i)=="A"){
-        rna+="U";
+];
+
+for (var i = 0; i < dna.length; i++) {
+    if (dna.charAt(i) == "A") {
+        rna += "U";
     }
-    if(dna.charAt(i)=="T"){
-        rna+="A";
+    if (dna.charAt(i) == "T") {
+        rna += "A";
     }
-    if(dna.charAt(i)=="G"){
-        rna+="C";
+    if (dna.charAt(i) == "G") {
+        rna += "C";
     }
-    if(dna.charAt(i)=="C"){
-        rna+="G";
+    if (dna.charAt(i) == "C") {
+        rna += "G";
     }
 }
 console.log(rna);
 
-if(rna.length%3!=0){
+if (rna.length % 3 != 0) {
     console.log("Can not translate to codon.")
-}else{
+} else {
     var codon = "";
     var count = 0;
     var num = 0;
@@ -49,7 +49,7 @@ if(rna.length%3!=0){
             num++;
         } else {
             codonarray.push(codon);
-            codon = ""; 
+            codon = "";
             count = 0;
         }
     }
@@ -57,17 +57,17 @@ if(rna.length%3!=0){
     console.log(codon);
     var exist = true;
 
-for (var i = 0; i < codonarray.length; i++) {
-    var found = false;
-    for (var j = 0; j < condons.length; j++) {
-        if (codonarray[i] == condons[j]) {
-            found = true;
+    for (var i = 0; i < codonarray.length; i++) {
+        var found = false;
+        for (var j = 0; j < condons.length; j++) {
+            if (codonarray[i] == condons[j]) {
+                found = true;
+                break;
+            }
+        }
+        if (found == false) {
+            exist = false;
             break;
         }
     }
-    if (found == false) {
-        exist = false;
-        break;
-    }
-}
 }
